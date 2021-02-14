9d8b8c81ec8608e2731c7f2e5316684811a092ba
Linkesh Kanna Velu <linkesh.kanna@bostonharborconsulting.com>
2021-02-14 15:29:55 +0530

    Test Commit for DWA Repo

--- MyBlogProject/MyBlogProject/asgi.py
+++ MyBlogProject/MyBlogProject/asgi.py
@@ -1,7 +1,7 @@
 """
-ASGI config for MyBlogProject project.
+ASGI config for MyBlogProject project
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+It exposes the ASGI callable as a module-level variable named ``application``
 
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyBlogProject.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MyBlogProject.settings")
 
 application = get_asgi_application()
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # django.web.apis
 Web APIs Developed Using Python Django
+
+Getting Started:
+python -m pip install django
+python -m django --version
+django-admin startproject MyBlogProject
+python .\MyBlogProject\manage.py runserver
+
+Applications and Routes:
+python manage.py startapp blog - This will create folder "blog" and py files in MyBlogProject
+create urls.py in blog folder
+add urlpatterns in urls.py
+create def home(request): in views.py
+add path("blog/", include("blog.urls")) in MyBlogProject/urls.py
