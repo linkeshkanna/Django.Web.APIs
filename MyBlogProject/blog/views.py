9648bac01155ebb4ee7f0315653093988b366e16
Linkesh Kanna Velu <linkesh.kanna@bostonharborconsulting.com>
2021-02-19 20:35:13 +0530

    Remove dummy data

--- MyBlogProject/blog/views.py
+++ MyBlogProject/blog/views.py
@@ -1,27 +1,14 @@
 from django.shortcuts import render
+from .models import Post
+
 # from django.http import HttpResponse
 
 # Create your views here.
 
-posts = [
-    {
-        'author': 'Linkesh',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Kanna',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
-
 def home(request):
     # return HttpResponse("<h1>Blog Home</h1>")
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, "blog/home.html", context)
 
